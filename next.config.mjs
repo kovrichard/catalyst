43e64b7a859cbe59bb14e9b521fa67cc8ce7a2cf
Richárd Kovács <richardkovacs.it@gmail.com>
2024-09-22 19:33:37 +0200

    build: add bcrypt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.externals = [...config.externals, "bcrypt"];
+        return config;
+    },
+};
 
 export default nextConfig;
