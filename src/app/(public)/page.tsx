5b567cad288931ad81fa2e7ac5dba4af0e822721
Richárd Kovács <richardkovacs.it@gmail.com>
2024-10-23 21:52:25 +0200

    feat(landing): set correct hero width

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -19,14 +19,14 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex flex-1 overflow-x-hidden">
+    <main className="flex flex-col items-center flex-1 overflow-x-hidden">
       <section className="flex flex-col items-center justify-center gap-8 max-w-7xl h-[80vh] mx-auto p-4">
         <div className="relative leading-none font-bold text-center text-neutral-600 space-y-2 text-[clamp(1.6rem,7vw,4.5rem)]">
           <p>A Starter Kit with</p>
           <FlipWords
             words={words}
             duration={2500}
-            className="text-center w-[70vw] text-neutral-900"
+            className="text-center w-[70vw] max-w-6xl text-neutral-900"
           />
           <p>Integration</p>
         </div>
