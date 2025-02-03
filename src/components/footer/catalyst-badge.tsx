4ea46f0463ade336375addfe796d331a3d455b2f
Richard Kovacs <richardkovacs.it@gmail.com>
2025-02-03 19:07:38 +0100

    feat: add utm parameters

--- src/components/footer/catalyst-badge.tsx
+++ src/components/footer/catalyst-badge.tsx
@@ -1,12 +1,19 @@
-import { headers } from "next/headers";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function CatalystBadge() {
-  const headersList = headers();
-  const domain = headersList.get("host");
+  const [source, setSource] = useState("");
+  const medium = "utm_medium=referral";
+  const campaign = "utm_campaign=made-with-badge";
+
+  useEffect(() => {
+    setSource(`utm_source=${window.location.hostname}`);
+  }, []);
 
   return (
     <a
-      href={`https://catalyst.richardkovacs.dev?ref=${domain}`}
+      href={`https://catalyst.richardkovacs.dev/?${source}&${medium}&${campaign}`}
       target="_blank"
       className="text-sm py-1 px-2 rounded flex items-center gap-1.5 shadow-md bg-card dark:bg-input border text-foreground hover:bg-input/50 transition-colors"
     >
