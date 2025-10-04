2228ab5abddcdbfd1a001575192e06e5187cc9b0
Richard Kovacs <richardkovacs.it@gmail.com>
2025-10-04 11:10:10 +0200

    fix(dashboard): make it fit page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-gray-100 gap-4">
+    <div className="flex flex-col items-center justify-center w-full flex-1 bg-gray-100 gap-4">
       <h1 className="text-6xl font-bold">Welcome here!</h1>
       <h2 className="text-xl font-medium">
         If you see this, you successfully authenticated.
