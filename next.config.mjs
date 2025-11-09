a40736bb9a14b3e3fb4777371cfe3c303768b444
Richard Kovacs <richardkovacs.it@gmail.com>
2025-11-09 07:25:38 +0100

    feat(docker): add prod container

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 import NextBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // This is required if you are using Docker
+    // output: "standalone",
+};
 
 const withBundleAnalyzer = NextBundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
