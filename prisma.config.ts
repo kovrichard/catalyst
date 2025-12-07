a86c81d27473803015c97635ceb01a57eaf2b23d
Richard Kovacs <richardkovacs.it@gmail.com>
2025-12-07 10:30:13 +0100

    fix(db-url): do not require direct url for generate

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,9 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+import { defineConfig } from "prisma/config";
+
+const envs = dotenv.config();
+dotenvExpand.expand(envs);
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
@@ -7,6 +11,6 @@ export default defineConfig({
     path: "./prisma/migrations",
   },
   datasource: {
-    url: env("DIRECT_DATABASE_URL"),
+    url: process.env.DIRECT_DATABASE_URL ?? "",
   },
 });
