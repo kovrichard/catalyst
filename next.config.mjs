2990346c40a0a63b2d593aa027229e924f62d9ed
Richard Kovacs <richardkovacs.it@gmail.com>
2025-12-13 08:06:07 +0100

    refactor(scripts): rename export env var to export_mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import NextBundleAnalyzer from '@next/bundle-analyzer';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Use `bun run build:standalone` if you are using Docker
-    output: process.env.EXPORT
+    output: process.env.EXPORT_MODE
 };
 
 const withBundleAnalyzer = NextBundleAnalyzer({
