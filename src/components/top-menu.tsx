f3a247d6bda1cc79dc01a375797f76f8831ca171
Richard Kovacs <richardkovacs.it@gmail.com>
2026-07-03 21:37:45 +0200

    style(dashboard): add border to top menu

--- src/components/top-menu.tsx
+++ src/components/top-menu.tsx
@@ -7,7 +7,7 @@ import NotificationMenu from "./notifications/notification-menu";
 
 export default function TopMenu() {
   return (
-    <div className="flex items-center justify-between bg-sidebar px-4 py-2">
+    <div className="flex items-center justify-between border-b bg-sidebar px-4 py-2">
       <SidebarTrigger />
       {/* @catalyst:auth-start */}
       <NotificationMenu />
